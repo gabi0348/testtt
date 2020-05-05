67cfc2bedcafb4928d5dd607405afbf9fb760c5b
gheorghe <gicu.jianu@gmail.com>
2020-05-05 13:42:12 +0300

    BackendInit

---
+++ Skills-Overflow-Backend/src/main/java/com/internshipProject/SkillsOverflowBackend/SkillsOverflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.internshipProject.SkillsOverflowBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsOverflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsOverflowBackendApplication.class, args);
+	}
+
+}
---
+++ Skills-Overflow-Backend/src/test/java/com/internshipProject/SkillsOverflowBackend/SkillsOverflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internshipProject.SkillsOverflowBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsOverflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
